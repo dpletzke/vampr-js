6a3377fd20c0e49f66c482cd9367bebe458e49c7
Dan Pletzke <dpletzke@gmail.com>
2020-10-03 19:19:19 +0000

    closest ancestor

--- vampire.js
+++ vampire.js
@@ -1,3 +1,12 @@
+
+// Who is a vampire's creator?
+// How many vampires has a vampire created?
+// How many vampires away from the original vampire is a vampire?
+// Who is the more senior vampire out of two vampires? (Who is closer to the original vampire)
+// Who is the closest common ancestor of two vampires?
+
+
+
 class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
@@ -10,22 +19,28 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    // console.log(this.creator);
+    if(this.creator) {
+      return 1 + this.creator.numberOfVampiresFromOriginal;
+    } else {
+      return 0;
+    }
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -35,9 +50,31 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
 
+  /* Helper function creating an array of ancestor objects or ancestor properties if an optional param is passed */
+  ancestorArray(prop) {
+    if (!this.creator) {
+      return prop ? [this[prop]] : [this];
+    } else if (prop) {
+      return [this[prop], ... this.creator.ancestorArray(prop)];
+    } else {
+      return [this, ... this.creator.ancestorArray()];
+    }
   }
+
+  closestCommonAncestor(vampire) {
+    const ancestorsNames = this.ancestorArray('name');
+    const compareAncestorsNames = vampire.ancestorArray('name');
+    const ancestors = this.ancestorArray();
+
+    const closestCommonName = ancestorsNames
+      .filter(name => compareAncestorsNames.includes(name))[0];
+    
+    return ancestors.find(vamp => vamp.name === closestCommonName);
+  }
+
+  
+
 }
 
 module.exports = Vampire;
